42011c85a472c8a18c9cf5f722da729014c160ec
YuriLuis <yuri.luizg@hotmail.com>
2019-12-02 18:25:28 -0300

    ProjetoCriado

---
+++ src/main/java/com/prova/recuperacao/provaRecuperacao/ProvaRecuperacaoApplication.java
@@ -0,0 +1,13 @@
+package com.prova.recuperacao.provaRecuperacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaRecuperacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaRecuperacaoApplication.class, args);
+	}
+
+}
